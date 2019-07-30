804a35a3cb646938620f95079d27356b38fb2b20
R. Zhao <zhao@rs-macbook-pro.lan>
2019-07-29 22:08:23 -0700

    database Updater

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Fund Tracking Web App
+
+## Database Updater
+
+The dbUpdater.ts runs once every weekday to record daily unit price of selected mutual fund on a document database.
---
+++ dbUpdater.ts
@@ -0,0 +1,69 @@
+import axios from 'axios';
+import cheerio =require('cheerio');
+import admin = require('firebase-admin');
+import dayjs = require('dayjs');
+import cron = require('node-cron');
+
+const serviceAccount = require('./fund-site-db-0fdeaa1f172b.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+var db = admin.firestore();
+
+const axiosInstance = axios.create({
+  headers: {'User-Agent': 
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+  });
+
+var getToday = () => {
+  return dayjs().format('YYYY-MM-DD');
+}
+
+var parseHTMLForPrice = (data : string): number => {
+  let $ = cheerio.load(data);
+  let fundPrice = $('span.change', '.primary-data-content').text().match(/[0-9]{2}\.[0-9]{2}/);
+  if (fundPrice) return Number(fundPrice[0]);
+  return NaN;
+  }
+
+async function fetchPrice(fundPriceURL : string) {
+  const response = await axiosInstance.get(fundPriceURL);
+  let fundPrice = parseHTMLForPrice(response['data']);
+  if(isNaN(fundPrice)){
+    throw "ERROR FETCHING PRICE FROM URL: " + fundPriceURL;
+    }
+  return fundPrice;
+}
+
+var delayRequest = () =>{
+  return new Promise(resolve => {
+    setTimeout(resolve, 3000, "Done");
+  });
+}
+
+async function updatePrice(){
+  try{
+    let documents = await db.collection('funds').get();
+    var docArray = documents.docs;
+    for(let i = 0; i < docArray.length; i++){
+      let fundPriceURL = docArray[i].get('site');
+      let today = getToday();
+      let docRef = docArray[i].ref;
+      await delayRequest();
+      let fundPrice = await fetchPrice(fundPriceURL);
+      let dbCurrentPrice = docArray[i].get('currentPrice');
+      let dbCurrentDay = docArray[i].get('currentDay');
+      if (fundPrice != dbCurrentPrice && dayjs(dbCurrentDay).isBefore(dayjs())) {
+        docRef.update({[today]: fundPrice});
+        docRef.update({currentDay: today});
+        docRef.update({currentPrice: fundPrice});
+      }
+    } 
+    db.collection('logs').doc('dbUpdater').update({[getToday()]: 'done' });
+  } catch(error) {
+    db.collection('logs').doc('dbUpdater').update({[getToday()]: error });
+  }
+}
+
+cron.schedule('00 30 18 * * 0-5', () => updatePrice());
